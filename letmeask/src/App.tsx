68caf3f1e0944afe099721206cb02718d4d14f6d
Felipe Sabino <felipedomingos2011@hotmail.com>
2021-06-25 19:45:31 -0300

    Dias 2 e 3

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,10 +1,23 @@
-import { Button } from './components/Button'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
-    <div>
-      <Button/>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" component={Home} exact/>
+          <Route path="/rooms/new" component={NewRoom} exact/>
+          <Route path="/rooms/:id" component={Room}/>
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 export default App;
